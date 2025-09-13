c2c6e4f88356ef2370121f5b4089b3f7b365e0f2
Matheus Altrao <matheusaltrao2@gmail.com>
2025-09-13 18:19:08 -0400

    ✨ feat(jobs/page): Adiciona a função `getJobById` para buscar detalhes de um trabalho por ID e atualiza `JobPage` para usar essa função

---
+++ src/actions/jobs/get-job-by-id.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+
+export async function getJobById(id: number) {
+  if (!id) {
+    throw new Error("getJobById ID is required");
+  }
+
+  try {
+    const job = await prisma.job.findUnique({
+      where: { id: id },
+    });
+    return job;
+  } catch (error) {
+    console.error("Failed to fetch job by ID:", error);
+    return null;
+  }
+}
--- src/app/(public)/(home)/[id]/page.tsx
+++ src/app/(public)/(home)/[id]/page.tsx
@@ -1,5 +1,5 @@
+import { getJobById } from "@/actions/jobs/get-job-by-id";
 import { JobDetails } from "@/components/secoes/job/job-details";
-import { JOBS } from "@/constants/jobs";
 import { notFound } from "next/navigation";
 
 interface JobPageProps {
@@ -8,17 +8,17 @@ interface JobPageProps {
   };
 }
 
-export default function JobPage({ params }: JobPageProps) {
-  const jobId = Number.parseInt(params.id);
-  const selectedJob = JOBS.find((j) => j.id === jobId);
+export default async function JobPage({ params }: JobPageProps) {
+  const paramsId = Number.parseInt(params.id);
+  const job = await getJobById(paramsId);
 
-  if (!selectedJob) {
+  if (!job) {
     notFound();
   }
 
   return (
     <div className="min-h-screen py-5">
-      <JobDetails job={selectedJob} />
+      <JobDetails job={job} />
     </div>
   );
 }
